f888950f38b4db978c7cac27d375c40db0091972
Isaacmathias07 <133376387+Isaacmathias07@users.noreply.github.com>
2024-02-29 12:24:54 -0300

    Add files via upload

---
+++ true.js
@@ -0,0 +1,3 @@
+let telefone = 43988005064;
+console.log("O telefone é " + String(telefone)); 
+// teremos a conversão do número 43988005064 para uma string "43988005064" e assim poderemos fazer a concatenação entre as strings
